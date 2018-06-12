595e0cd23913388b9bcb1f15cca3144222f488bf
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-12 19:39:01 +0530

    Add Test GET Todos

--- server/server.test.js
+++ server/server.test.js
@@ -4,8 +4,13 @@ const request=require('supertest');
 const {app}=require('./server');
 const {Todo}=require('./mongoose/todo');
 
+const todos=[{text:"dummy text 1"},{text:"dummy text 2"}];
+
 beforeEach((done)=>{
-   Todo.remove({}).then(()=> done()).catch((e)=>done(e));
+   Todo.remove({}).then(()=>{
+   	Todo.insertMany(todos);
+   }).
+   then(()=> done()).catch((e)=>done(e));
 });
 
 describe('POST/todos',()=>{
@@ -23,7 +28,7 @@ describe('POST/todos',()=>{
                         	return done(err);
                         }
 
-                      Todo.find().then((todos)=>{
+                      Todo.find({text}).then((todos)=>{
                          expect(todos.length).toBe(1);
                          expect(todos[0].text).toBe(text);
                          done();
@@ -42,9 +47,20 @@ describe('POST/todos',()=>{
                          	return done(err);
                          }
                          Todo.find().then((todos)=>{
-                           expect(todos.length).toBe(0);
+                           expect(todos.length).toBe(2);
                            done();
                          }).catch((e)=>done(e));
 		            });
+	});
+});
+
+describe('GET /todo',()=>{
+	it('should get all todos',(done)=>{
+		request(app).get('/todo')
+		            .expect(200)
+		            .expect((res)=>{
+		            	expect(res.body.todos.length).toBe(2);
+		            })
+		            .end(done);
 	});
 });
