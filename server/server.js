8a551b289eae32485c251110cbd1a63acb27fae1
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-12 19:24:41 +0530

    Add GET Todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,13 @@ app.post('/todo',(req,res)=>{
   
 });
 
+app.get('/todo',(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	}).catch((e)=>{
+        res.status(400).send(e);
+	});
+});
 app.listen(3000,()=>{
    console.log("connected");
 });
